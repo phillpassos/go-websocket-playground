707b47f4188dee7abf4ea4654cb28915403f36b1
Phillip Miguel dos Passos <phillipmigueldospassos@gmail.com>
2017-09-20 11:56:47 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type msg struct {
+	msg string
+}
+
+func main() {
+	http.HandleFunc("/echo", wsHandler)
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Origin") != "http://"+r.Host {
+		http.Error(w, "Fora da origem", 403)
+		return
+	}
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Erro ao abrir conexao", http.StatusBadRequest)
+	}
+
+	conn.SetCloseHandler(closeHandler)
+
+	go echo(conn)
+}
+
+func closeHandler(code int, text string) error {
+	fmt.Println("Cliente desconectou")
+	return errors.New("")
+}
+
+func echo(conn *websocket.Conn) {
+	for {
+		var m map[string]interface{}
+
+		err := conn.ReadJSON(&m)
+		if err != nil {
+			conn.Close()
+			fmt.Println("Erro ao ler json.", err)
+			return
+		}
+
+		fmt.Printf("Mensagem: %#v\n", m)
+
+		if err = conn.WriteJSON(m); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
